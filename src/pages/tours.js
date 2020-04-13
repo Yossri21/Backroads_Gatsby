c785ddd1100fbbfa13e8e722b77995a079bdda1b
Yosri21 <yosriasfour@gmail.com>
2020-04-13 16:11:50 +0100

    styled component

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
 import Layout from "../components/Layout"
-
+import Button from '../examples/Button'
 const tours = () => {
     return (
         <Layout>
-            <h4> ijoijoj</h4>
+            <h4> helloo from tour</h4>
+        <div>
+            <Button big> Hi again </Button>
+            <Button color="yellow"> second button </Button>
+            </div>
         </Layout>
        
     )

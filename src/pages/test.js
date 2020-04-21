4f3702fda8a705cb09bc5df3e003d9a6a09af3f4
Yosri21 <yosriasfour@gmail.com>
2020-04-21 13:40:38 +0100

    tes

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const test = () => {
+    return (
+        <div>
+            <h4> hello error </h4>
+            <h1> hak </h1>
+        </div>
+    )
+}
+
+export default test
